4d1b166b7116bacde13f8fd9e7b231fdd8c7031c
edevPires <133178329+edevPires@users.noreply.github.com>
2024-09-27 19:49:25 -0300

    Commit 2

--- p1/lib/main.dart
+++ p1/lib/main.dart
@@ -13,7 +13,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Cadastro extends StatefulWidget {
+  final EventosRepository eventos;
+  Cadastro({required this.eventos});
+
+  @override
+  State<Cadastro> createState() => _CadastroState(eventos: eventos);
+}
+
+class _CadastroState extends State<Cadastro> {
+  final TextEditingController tituloController = TextEditingController();
+  final TextEditingController dataController = TextEditingController();
+  final EventosRepository eventos;
+
+  _CadastroState({required this.eventos});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de Evento'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Título do Evento',
+                  border: OutlineInputBorder(),
+                ),
+                controller: tituloController,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Data do Evento (dd/mm/yyyy)',
+                  border: OutlineInputBorder(),
+                ),
+                controller: dataController,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    eventos.addEvento(Evento(
+                        titulo: tituloController.text,
+                        data: dataController.text));
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text('Salvar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class Principal extends StatelessWidget {
+  final EventosRepository eventos = EventosRepository();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +85,49 @@ class Principal extends StatelessWidget {
         title: Text('Agenda de Eventos'),
       ),
       body: Center(
-        child: Text('Bem-vindo à Agenda de Eventos!'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Cadastro(eventos: eventos),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 20.0),
+                  child: Text("Cadastrar Evento"),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class Evento {
+  String titulo;
+  String data;
+
+  Evento({required this.titulo, required this.data});
+}
+
+class EventosRepository {
+  final List<Evento> eventos = [];
+
+  void addEvento(Evento e) {
+    eventos.add(e);
+  }
+
+  List<Evento> getEventos() {
+    return eventos;
   }
 }
